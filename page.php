53b7312cbbce8a41121db0b165f56b8d6f41420c
Zhong Chai <Z.CHAI@labs.clark.edu>
2019-02-19 12:48:22 -0800

    Changes made in class

--- page.php
+++ page.php
@@ -11,15 +11,18 @@
 </head>
 <body>
 <div class="container">
-    <section>
-        <h1>Evergreen</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
-        <h2>Camas</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
+    <section class="bg-secondary text-white p-4">
+        <!-- This class turns the image into a block-level element, and margin-auto to center it -->
+        <img class="d-block mx-auto py-4" src="img/200px-Clark_College_logo.png" alt="Clark College Logo">
+        <div class="alert alert-info">What's this!</div>
+        <div class="display-3">OwO</div>
+        <p class="lead text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
+        <h2 class="mt-4">UwU</h2>
+        <p class="mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
         <h3>Vanmcouver</h3>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
         <h4>Comsewogue</h4>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
+        <p class="blockquote font-italic border-left pl-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
     </section>
 </div>
 
