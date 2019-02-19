7902a6559f3e06e32de5efa16e37c5f6879634b1
Zhong Chai <Z.CHAI@labs.clark.edu>
2019-02-19 11:02:58 -0800

    Initial commit

---
+++ page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Page Title</title>
+</head>
+<body>
+<div class="container">
+    <section>
+        <h1>Evergreen</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
+        <h2>Camas</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
+        <h3>Vanmcouver</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
+        <h4>Comsewogue</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
+    </section>
+</div>
+
+<!-- jQuery -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<!-- Bootstrap JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,15 @@
+# CTEC 127 Template
+
+Course template for CTEC 127
+
+## File and Folder Structure
+
+- A PHP page named **page.php** - this file contains all of the HTML to get you started. It also contains a link to the CSS style sheet for the Bootstrap framework. Here's a link to a cheat sheet (Links to an external site.)Links to an external site. that has all of the Bootstrap classes that you can use to style your work.
+- A folder named **css** - In this folder a file name style.css
+  - a file named **bootstrap.min.css** which is the CSS file for Bootstrap 4
+- A folder named **img** - this folder has a sample .png file in it and should be used to store images in.
+- A folder named **js** - this folder is used to store JavaScript in.
+  - in this folder is **bootstrap.min.js** which is for Bootstrap
+  - in this folder is **jquery-3.3.1.min.js** which is the jQuery library used by some of jQuery's compoments.
+- A folder named **inc** - this contains a file named **placehoder.inc.php** and should be used to stored server-side scripts that do not produce output. Libraries, includes, classes, etc.
+- A folder named **doc** - this folder contains a file named **documentation.md** and should contain documentation about the project like Markdown documents, office documents and PDFs, that aren't part of normal page navigation.
